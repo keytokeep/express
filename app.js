d1d0f6696dd1969b1e54481013bd1f9a1d8173f1
sereg <sereg.lycoris@gmail.com>
2025-01-28 11:36:59 +0900

    add env

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
 var nodeRouter = require('./routes/node');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/notes_from_b', notes_from_bRouter);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
